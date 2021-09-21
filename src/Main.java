bb4ed8398b90f0e8b0cda0424ec5bc81df444449
Csaba Karsai <csaba@karsai.info>
2021-09-21 16:13:38 +0200

    implemented unit tests for rotor type I

--- src/Main.java
+++ src/Main.java
@@ -8,38 +8,7 @@ import src.enigma.Rotor;
 public class Main{
     public static void main(String[] args) {
 
-        Reflector reflector = new Reflector("C");
-        System.out.println("mapping: " + reflector.getMapping());
-        char inputChar = 'A';
-        int input = toInt(inputChar);
-        System.out.println("input letter: " + toChar(input));
-        int output = reflector.encode(input);
-        System.out.println("output letter: " + toChar(output));
-
-        /*
-        Rotor rotor3 = new Rotor("V", 2, 1);
-        System.out.println("mapping: " + rotor3.getMapping());
-        char inputChar = 'A';
-        int input = toInt(inputChar);
-        System.out.println("input letter: " + toChar(input));
-        int output = rotor3.encode(input);
-        System.out.println("output letter: " + toChar(output));
-        */
-
-        /*
-        DEBUGGING
-
-        System.out.println(mappingRotorI);
-        System.out.println(toChar(mappingRotorINumbers.get(1)));
-        System.out.println(toChar(mappingRotorINumbers.get(14)));
-        System.out.println(toChar(mappingRotorINumbers.get(26)));
-
-        System.out.println(mappingRotorI);
-        mappingRotorI.put('A', 'B');
-        System.out.println(mappingRotorI);
-        setEntry(mappingRotorI, 'A', 'C');
-        System.out.println(mappingRotorI);
-        */
+        
 
     }
 
@@ -49,10 +18,6 @@ public class Main{
     
     public static char toChar(int number){
         return (char) (number + 64);
-    }
-    
-    public static void setEntry(HashMap<Character, Character> mapping, char key, char value){
-        mapping.put(key, value);
     }
 
 }
--- test/UnitTests.java
+++ test/UnitTests.java
@@ -1,17 +1,61 @@
 package test;
 
+import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Test;
+import src.enigma.Reflector;
+import src.enigma.Rotor;
 
 public class UnitTests {
     
     @Test
-    public void simpleTest(){
-        int a = 2;
-        int b = 3;
-        int c = a + b;
-        assertEquals(5, c);
+    public void testRotorIEncode(){
+        Rotor rotorI = new Rotor("I", 1, 1);
+        char inputChar = 'A';
+        int inputInt = toInt(inputChar);
+        int outputInt = rotorI.encode(inputInt);
+        char outputChar = toChar(outputInt);
+        assertEquals('E', outputChar);
+
+        rotorI = new Rotor("I", 2, 1);
+        outputInt = rotorI.encode(inputInt);
+        outputChar = toChar(outputInt);
+        assertEquals('J', outputChar);
+
+        rotorI = new Rotor("I", 1, 2);
+        outputInt = rotorI.encode(inputInt);
+        outputChar = toChar(outputInt);
+        assertEquals('K', outputChar);
+
+        rotorI = new Rotor("I", 17, 8);
+        outputInt = rotorI.encode(inputInt);
+        outputChar = toChar(outputInt);
+        assertEquals('Q', outputChar);
+
+        rotorI = new Rotor("I", 9, 15);
+        inputChar = 'T';
+        inputInt = toInt(inputChar);
+        outputInt = rotorI.encode(inputInt);
+        outputChar = toChar(outputInt);
+        assertEquals('C', outputChar);
+    }
+
+    @Test
+    public void testReflectorCEncode(){
+        Reflector reflectorC = new Reflector("C");
+        char inputChar = 'A';
+        int inputInt = toInt(inputChar);
+        int outputInt = reflectorC.encode(inputInt);
+        char outputChar = toChar(outputInt);
+        assertEquals('F', outputChar);
+    }
+
+    public int toInt(char letter){
+        return Character.getNumericValue(letter) - 9;
+    }
+    
+    public char toChar(int number){
+        return (char) (number + 64);
     }
 
 }
