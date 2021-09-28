8df7157369bbec159072c0862db783d622442069
Csaba Karsai <csaba@karsai.info>
2021-09-28 10:07:10 +0200

    changed integration to use arbitrary settings

--- test/IntegrationTests.java
+++ test/IntegrationTests.java
@@ -11,44 +11,47 @@ public class IntegrationTests {
 
     @Test
     public void testEnigmaEncodeSingleLetter(){
-        HashMap<Integer,Integer> plugboardDefault = new HashMap<Integer,Integer>(
+
+        HashMap<Integer,Integer> plugboard = new HashMap<Integer,Integer>(
             Map.ofEntries(
-                entry(1, 1),
+                entry(1, 4),
                 entry(2, 2),
-                entry(3, 3),
-                entry(4, 4),
-                entry(5, 5),
-                entry(6, 6),
-                entry(7, 7),
-                entry(8, 7),
-                entry(9, 8),
-                entry(10, 10),
-                entry(11, 11),
-                entry(12, 12),
+                entry(3, 14),
+                entry(4, 1),
+                entry(5, 20),
+                entry(6, 12),
+                entry(7, 9),
+                entry(8, 8),
+                entry(9, 7),
+                entry(10, 22),
+                entry(11, 26),
+                entry(12, 6),
                 entry(13, 13),
-                entry(14, 14),
+                entry(14, 3),
                 entry(15, 15),
-                entry(16, 16),
-                entry(17, 17),
+                entry(16, 21),
+                entry(17, 25),
                 entry(18, 18),
                 entry(19, 19),
-                entry(20, 20),
-                entry(21, 21),
-                entry(22, 22),
-                entry(23, 23),
-                entry(24, 24),
-                entry(25, 25),
-                entry(26, 26)
+                entry(20, 5),
+                entry(21, 16),
+                entry(22, 10),
+                entry(23, 24),
+                entry(24, 23),
+                entry(25, 17),
+                entry(26, 11)
             )
         );
 
         Enigma enigma = new Enigma();
-        enigma.setPlugboard(plugboardDefault);
-        enigma.setRightRotor("III", 1, 1);
-        enigma.setMiddleRotor("II", 1, 1);
-        enigma.setLeftRotor("I", 1, 1);
+        enigma.setPlugboard(plugboard);
+        enigma.setRightRotor("V", 25, 22);
+        enigma.setMiddleRotor("II", 8, 13);
+        enigma.setLeftRotor("IV", 20, 10);
         enigma.setReflector("B");
-        assertEquals('U', enigma.encode('A'));
+        assertEquals('J', enigma.encode('A'));
+        assertEquals('I', enigma.encode('B'));
+        assertEquals('E', enigma.encode('C'));
         
     }
 
